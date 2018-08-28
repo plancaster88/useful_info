0c60b2dd82590ccb5dba5360ad44efb5cdf717e5
Philip Lancaster <33505124+plancaster88@users.noreply.github.com>
2018-08-28 09:53:48 -0500

    Update sql_snippets.sql

--- sql_snippets.sql
+++ sql_snippets.sql
@@ -1,3 +1,30 @@
+
+/***********Find word in any column or table***********/
+SELECT  
+  'Planreport_qnxt_la.'+s.[name]+'.'+ t.[name]+'.'+c.[name] ,
+        s.[name]            'Schema',
+        t.[name]            'Table',
+        c.[name]            'Column',
+        d.[name]            'Data Type',
+        d.[max_length]      'Max Length',
+        d.[precision]       'Precision',
+        c.[is_identity]     'Is Id',
+        c.[is_nullable]     'Is Nullable',
+        c.[is_computed]     'Is Computed',
+        d.[is_user_defined] 'Is UserDefined',
+        t.[modify_date]     'Date Modified',
+        t.[create_date]     'Date created'
+FROM        sys.schemas s
+INNER JOIN  sys.tables  t
+ON s.schema_id = t.schema_id
+INNER JOIN  sys.columns c
+ON t.object_id = c.object_id
+INNER JOIN  sys.types   d
+ON c.user_type_id = d.user_type_id
+WHERE 
+c.name like '%Fee%'     --Type the column you are searching for here. I use a wild card if I do not know the exact column name.
+ and t.[name]   like '%fee%'  
+
 /****************  Format Date as YYYYMM *******************/
 	select convert(char(6), getdate(), 112) ;
   
